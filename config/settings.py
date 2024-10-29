d18ab6cd61bc01cbf461bfb688a9a75cb6869674
winteringT1111 <tmdgus000415@gmail.com>
2024-10-29 11:49:39 +0900

    update

--- config/settings.py
+++ config/settings.py
@@ -129,9 +129,9 @@ CSRF_TRUSTED_ORIGINS = ['https://winteringtheory.site']
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [ BASE_DIR / 'static/']
 
-STATIC_ROOT = BASE_DIR / 'static/'
+#STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
