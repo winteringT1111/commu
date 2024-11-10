240a06db92c07e981715cf8023e0acb10f035439
winteringT1111 <tmdgus000415@gmail.com>
2024-11-11 01:28:05 +0900

    d

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from users.models import CharInfo
-from member.models import Inventory, Inventory_magic, Characters
+from member.models import Inventory, Inventory_magic, Characters,Gift
 from store.models import Item, Item_magic
 
 # Register your models here.
@@ -10,3 +10,4 @@ admin.site.register(Item_magic)
 admin.site.register(Inventory)
 admin.site.register(Inventory_magic)
 admin.site.register(Characters)
+admin.site.register(Gift)
